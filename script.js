6f28530f88afa0b9736a13181aeee201aa4faf86
Muhammad Azeem <37941410+AzeemSup@users.noreply.github.com>
2020-07-18 15:11:58 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+let userInput;
+let budget = 0;
+let totalAmount = 0;
+let mangoQuantity = 0;
+let mangoPrice = 15;
+
+function inputValue(){
+    if(userInput == null){
+        alert("Please enter some balance");
+    }
+    else{
+        userInput = document.querySelector("input").value;
+        console.log(userInput);
+        var inputItem = document.querySelector("#user-balance");
+        alert("The balance you entered: " +userInput+ " PKR");
+        inputItem.innerHTML = userInput;
+    }
+}
+
+function addMango() {
+    if (totalAmount <= budget) {
+        budget = userInput;
+        document.getElementById("quantityOfMango").innerHTML = ++mangoQuantity;
+        totalAmount = mangoPrice * mangoQuantity;
+        document.getElementById("total").innerHTML = totalAmount;
+        budget = budget - mangoPrice;
+        document.getElementById("balance").innerHTML = userInput - totalAmount;
+    }
+    else {
+        alert("You can't purchase more!")
+    }
+}
+
+function subMango() {
+    if (mangoQuantity > 0) {
+        budget = userInput;
+        document.getElementById("quantityOfMango").innerHTML = --mangoQuantity;
+        totalAmount = mangoPrice * mangoQuantity;
+        document.getElementById("total").innerHTML = totalAmount;
+        budget = budget + mangoPrice;
+        document.getElementById("balance").innerHTML = userInput - totalAmount;
+    }
+    else{
+       alert("Please buy any fruit");
+    }
+}
